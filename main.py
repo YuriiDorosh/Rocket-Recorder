396c4a19b21ae319680e470f094985454ea520dd
yurii2 <yurannn7777@gmail.com>
2023-06-11 15:10:26 +0300

    add static method decorators

--- main.py
+++ main.py
@@ -1,160 +1,3 @@
-# from tkinter import *
-# import os
-# import platform
-# import datetime
-# import subprocess
-# import threading
-# import pyscreenshot as ImageGrab
-# from tkinter.filedialog import *
-# from tkinter import messagebox
-# from screen_recorder_sdk import screen_recorder
-# import pyautogui
-#
-#
-# # Reasking if User want to leave the app
-# def on_closing():
-#     if messagebox.askokcancel("Exit", "Do You want to exit the App?\n Data may be corrupted"):
-#         root.destroy()
-#
-#
-# # Function to start recording on Linux
-# def record_linux():
-#     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-#     file_name = f"video_{timestamp}.mp4"
-#
-#     subprocess.run([
-#         "ffmpeg",
-#         "-video_size", "1920x1080",
-#         "-framerate", "30",
-#         "-f", "x11grab",
-#         "-i", ":0.0",
-#         file_name
-#     ])
-#
-#
-# # Function to start recording
-# def start_recording():
-#     if platform.system() == "Linux":
-#         threading.Thread(target=record_linux).start()
-#     else:
-#         screen_recorder.enable_dev_log()
-#         params = screen_recorder.RecorderParams()
-#         screen_recorder.init_resources(params)
-#         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-#         file_name = f"video_{timestamp}.mp4"
-#         screen_recorder.start_video_recording(os.path.join(f"{file_name}"), 30, 8000000, True)
-#
-#
-# # Function to stop recording
-# def stop_recording():
-#     if platform.system() == "Linux":
-#         subprocess.run(["pkill", "ffmpeg"])
-#     else:
-#         screen_recorder.stop_video_recording()
-#         screen_recorder.free_resources()
-#
-#
-# # Function to open the folder
-# def open_folder():
-#     if platform.system() == "Linux":
-#         subprocess.run(["xdg-open", os.getcwd()])
-#     else:
-#         path = os.path.realpath(os.getcwd())
-#         os.startfile(path)
-#
-#
-# # Function to capture a screenshot
-# def make_screenshot():
-#     if platform.system() == "Linux":
-#         screenshot = ImageGrab.grab()
-#         save_path = asksaveasfilename()
-#         screenshot.save(save_path + "_screenshot.png")
-#     else:
-#         myscreenshot = pyautogui.screenshot()
-#         save_path = asksaveasfilename()
-#         myscreenshot.save(save_path + "_screenshot.png")
-#
-#
-# # Function to open the tips window
-# def start_window_1():
-#     new_window_1 = Toplevel(root)
-#     new_window_1.geometry("400x300")
-#     new_window_1.title("Tips")
-#     new_window_1.resizable(False, False)
-#     new_window_1.wm_attributes("-topmost", 1)
-#     label2 = Label(new_window_1, image=bg)
-#     label2.place(x=0, y=0)
-#
-#
-# # head
-# root = Tk()
-# root.geometry("1200x600")
-# root.title("rocket recorder")
-# root.config(bg="#F8F8FF")
-# root.protocol("WM_DELETE_WINDOW", on_closing)
-# root.resizable(False, False)
-#
-# img = PhotoImage(file="photo//space.png")
-#
-# app_label = Label(
-#     root,
-#     text="Rocket recorder",
-#     image=img,
-#     bg="#A9A9A9",
-#     font=("rockwell", 26, "bold"),
-#     padx=500,
-#     pady=32,
-# )
-# app_label.grid(columnspan=3)
-# bg = PhotoImage(file="for_new_window//wininfo.png")
-#
-# # Buttons
-# photo = PhotoImage(file=os.path.join("buttons//start.png"))
-# record_btn = Button(
-#     root,
-#     command=start_recording,
-#     image=photo,
-#     bg="#F8F8FF",
-#     activebackground="#F8F8FF",
-#     bd=0,
-# )
-# record_btn.grid(row=5, column=0, pady=100)
-#
-# photo2 = PhotoImage(file=os.path.join("buttons//stop.png"))
-# stop_btn = Button(
-#     root,
-#     command=stop_recording,
-#     image=photo2,
-#     bg="#F8F8FF",
-#     activebackground="#F8F8FF",
-#     bd=0,
-# )
-# stop_btn.grid(row=5, column=1, pady=3)
-#
-# photo3 = PhotoImage(file=os.path.join("buttons//folder.png"))
-# folder_btn = Button(
-#     root,
-#     command=open_folder,
-#     image=photo3,
-#     bg="#F8F8FF",
-#     activebackground="#F8F8FF",
-#     bd=0,
-# )
-# folder_btn.grid(row=5, column=2, pady=3)
-#
-# photo4 = PhotoImage(file=os.path.join("buttons//tip.png"))
-# tip_btn = Button(root, command=start_window_1, image=photo4, bg="#A9A9A9", bd=2).place(
-#     x=1148, y=20
-# )
-#
-# photo5 = PhotoImage(file=os.path.join("buttons//screen1.png"))
-# scr_btn = Button(root, command=make_screenshot, image=photo5, bg="#A9A9A9", bd=2).place(
-#     x=50, y=60
-# )
-#
-# root.mainloop()
-
-
 from tkinter import *
 import os
 import platform
@@ -167,8 +10,23 @@ from tkinter import messagebox
 from screen_recorder_sdk import screen_recorder
 import pyautogui
 
+
+def open_folder():
+    """
+    Open the folder containing the recorded videos.
+    """
+    if platform.system() == "Linux":
+        subprocess.run(["xdg-open", os.getcwd()])
+    else:
+        path = os.path.realpath(os.getcwd())
+        os.startfile(path)
+
+
 class RocketRecorder:
     def __init__(self):
+        """
+        Initialize the RocketRecorder application.
+        """
         self.root = Tk()
         self.root.geometry("1200x600")
         self.root.title("rocket recorder")
@@ -198,6 +56,9 @@ class RocketRecorder:
         self.create_buttons()
 
     def create_buttons(self):
+        """
+        Create the buttons in the application.
+        """
         record_btn = Button(
             self.root,
             command=self.start_recording,
@@ -220,7 +81,7 @@ class RocketRecorder:
 
         folder_btn = Button(
             self.root,
-            command=self.open_folder,
+            command=open_folder,
             image=self.folder_btn_image,
             bg="#F8F8FF",
             activebackground="#F8F8FF",
@@ -228,60 +89,100 @@ class RocketRecorder:
         )
         folder_btn.grid(row=5, column=2, pady=3)
 
-        tip_btn = Button(self.root, command=self.start_window_1, image=self.tip_btn_image, bg="#A9A9A9", bd=2)
+        tip_btn = Button(
+            self.root,
+            command=self.start_window_1,
+            image=self.tip_btn_image,
+            bg="#A9A9A9",
+            bd=2,
+        )
         tip_btn.place(x=1148, y=20)
 
-        scr_btn = Button(self.root, command=self.make_screenshot, image=self.scr_btn_image, bg="#A9A9A9", bd=2)
+        scr_btn = Button(
+            self.root,
+            command=self.make_screenshot,
+            image=self.scr_btn_image,
+            bg="#A9A9A9",
+            bd=2,
+        )
         scr_btn.place(x=50, y=60)
 
     def start_window_1(self):
+        """
+        Open the "Tips" window.
+        """
         new_window_1 = Toplevel(self.root)
         new_window_1.geometry("400x300")
         new_window_1.title("Tips")
+        if platform.system() == "Windows":
+            new_window_1.iconbitmap("for_new_window//icoo.ico")
         new_window_1.resizable(False, False)
         new_window_1.wm_attributes("-topmost", 1)
         label2 = Label(new_window_1, image=self.bg)
         label2.place(x=0, y=0)
 
-    def start_recording(self):
-        if platform.system() == "Linux":
-            threading.Thread(target=self.record_linux).start()
-        else:
-            screen_recorder.enable_dev_log()
-            params = screen_recorder.RecorderParams()
-            screen_recorder.init_resources(params)
-            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            file_name = f"video_{timestamp}.mp4"
-            screen_recorder.start_video_recording(os.path.join(f"{file_name}"), 30, 8000000, True)
+    @staticmethod
+    def record_windows():
+        """
+        Record the screen using the screen_recorder_sdk on Windows.
+        """
+        screen_recorder.enable_dev_log()
+        params = screen_recorder.RecorderParams()
+        screen_recorder.init_resources(params)
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = f"video_{timestamp}.mp4"
+        screen_recorder.start_video_recording(
+            os.path.join(f"{file_name}"), 30, 8000000, True
+        )
 
-    def record_linux(self):
+    @staticmethod
+    def record_linux():
+        """
+        Record the screen using ffmpeg on Linux.
+        """
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         file_name = f"video_{timestamp}.mp4"
 
-        subprocess.run([
-            "ffmpeg",
-            "-video_size", "1920x1080",
-            "-framerate", "30",
-            "-f", "x11grab",
-            "-i", ":0.0",
-            file_name
-        ])
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-video_size",
+                "1920x1080",
+                "-framerate",
+                "30",
+                "-f",
+                "x11grab",
+                "-i",
+                ":0.0",
+                file_name,
+            ]
+        )
 
-    def stop_recording(self):
+    def start_recording(self):
+        """
+        Start the screen recording based on the platform.
+        """
+        if platform.system() == "Linux":
+            threading.Thread(target=self.record_linux).start()
+        else:
+            threading.Thread(target=self.record_windows).start()
+
+    @staticmethod
+    def stop_recording():
+        """
+        Stop the screen recording based on the platform.
+        """
         if platform.system() == "Linux":
             subprocess.run(["pkill", "ffmpeg"])
         else:
             screen_recorder.stop_video_recording()
             screen_recorder.free_resources()
 
-    def open_folder(self):
-        if platform.system() == "Linux":
-            subprocess.run(["xdg-open", os.getcwd()])
-        else:
-            path = os.path.realpath(os.getcwd())
-            os.startfile(path)
-
-    def make_screenshot(self):
+    @staticmethod
+    def make_screenshot():
+        """
+        Capture a screenshot of the screen.
+        """
         if platform.system() == "Linux":
             screenshot = ImageGrab.grab()
             save_path = asksaveasfilename()
@@ -292,16 +193,21 @@ class RocketRecorder:
             myscreenshot.save(save_path + "_screenshot.png")
 
     def on_closing(self):
+        """
+        Handle the closing of the application window.
+        """
         if messagebox.askokcancel(
                 "Exit", "Do You want to exit the App?\n Data may be corrupted"
         ):
             self.root.destroy()
 
     def run(self):
+        """
+        Run the RocketRecorder application.
+        """
         self.root.mainloop()
+
 
 if __name__ == "__main__":
     app = RocketRecorder()
     app.run()
-
-
