29540369552b85ea9acbdeebb911c968217e4dc3
yurii <yuriydorosh2005@gmail.com>
2023-06-11 19:40:57 +0300

    added pauses

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 # Make virtual environment
 python3 -m venv env
 
+sleep 10
+
 if [ $? -ne 0 ]; then
   echo "Error: Failed to create virtual environment."
   exit 1
@@ -11,6 +13,8 @@ fi
 
 # Activation of the virtual environment
 source env/bin/activate
+
+sleep 10
 
 if [ $? -ne 0 ]; then
   echo "Error: Failed to activate the virtual environment."
@@ -20,6 +24,8 @@ fi
 # Installing dependencies from requirements.txt
 pip install -r requirements.txt
 
+sleep 180
+
 if [ $? -ne 0 ]; then
   echo "Error: Unable to set dependency."
   exit 1
@@ -28,6 +34,9 @@ fi
 
 # Start the project
 cd ..
+
+sleep 5
+
 python3 Rocket-Recorder
 
 # Exit the script
